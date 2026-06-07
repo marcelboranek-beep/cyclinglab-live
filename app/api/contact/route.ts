dde59fd34128c54201b62c74ab599edac019316e
Marcel Boránek <info@customcyclinglab.cz>
2026-06-07 18:32:33 +0200

    Test contact endpoint

--- app/api/contact/route.ts
+++ app/api/contact/route.ts
@@ -3,6 +3,8 @@ import { Resend } from "resend";
 const resend = new Resend(process.env.RESEND_API_KEY);
 
 export async function POST(req: Request) {
+  console.log("API CONTACT HIT");
+
   try {
     const body = await req.json();
 
